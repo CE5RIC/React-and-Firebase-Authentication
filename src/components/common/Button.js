fc645da69c1b76fc58732af7f9c1241c4f12e097
CE5RIC <cedricdierckx5@hotmail.com>
2022-09-27 16:40:27 +0200

    handleaction method passed into button component as props

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -2,11 +2,11 @@ import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 
-export default function BasicButtons({title}) {
+export default function BasicButtons({title, handleAction}) {
     return (
         <Stack spacing={2} direction="row">
 
-            <Button variant="contained">{title}</Button>
+            <Button variant="contained">{title} handleAction={handleAction}</Button>
         </Stack>
     );
 }
