28966ddc20e89ac72f5019dbb89df851fcd4f770
CE5RIC <cedricdierckx5@hotmail.com>
2022-09-27 16:35:20 +0200

    states created for email and password

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
 import Form from './components/common/Form'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {useState} from "react";
 
 function App() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleAction = () => {
+
+    }
+
   return (
       <Router>
     <div className="App">
         <>
             <Routes>
-                <Route path='/login' element={<Form title="Login" />} />
-                <Route path='/register' element={<Form title="Register" />} />
+                <Route path='/login' element={<Form title="Login" setEmail={setEmail} setPassword={setPassword} handleAction={() => handleAction()} />} />
+                <Route path='/register' element={<Form title="Register" setEmail={setEmail} setPassword={setPassword} handleAction={() => handleAction()} />} />
             </Routes>
         </>
     </div>
