4fc11326c409e0c748cb6a88bd1ee54c27c5eab0
CE5RIC <cedricdierckx5@hotmail.com>
2022-09-27 17:24:16 +0200

    minor tweak

---
+++ src/firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBzV-yqhfnQVgQt--Nj3kEDqR9rXkyk7hw",
+    authDomain: "reactfirebaseauthenticat-f4b31.firebaseapp.com",
+    projectId: "reactfirebaseauthenticat-f4b31",
+    storageBucket: "reactfirebaseauthenticat-f4b31.appspot.com",
+    messagingSenderId: "843515088762",
+    appId: "1:843515088762:web:9d6799cf022b009e17d0c3",
+    measurementId: "G-N4H9EPJ6YM"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
