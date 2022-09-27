7a2ed090fae4949431c4475f20a6fd48907f2b66
CE5RIC <cedricdierckx5@hotmail.com>
2022-09-27 16:38:42 +0200

    setpassword and setemail pass to form component

--- src/components/common/Form.js
+++ src/components/common/Form.js
@@ -3,7 +3,7 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from './Button'
 
-export default function BasicTextFields({title}) {
+export default function BasicTextFields({title, setPassword, setEmail, handleAction}) {
     return (
         <div>
             <div className="heading-container">
@@ -20,8 +20,8 @@ export default function BasicTextFields({title}) {
                 noValidate
                 autoComplete="off"
             >
-                <TextField id="email" label="Enter the Email" variant="outlined" />
-                <TextField id="password" label="Enter the Password" variant="outlined" />
+                <TextField id="email" label="Enter the Email" variant="outlined" onChange={(e) => setEmail(e.target.value)} />
+                <TextField id="password" label="Enter the Password" variant="outlined" onChange={(e) => setPassword(e.target.value)} />
             </Box>
             <Button title={title} />
         </div>
