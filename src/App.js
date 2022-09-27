d671bf30eb40c953bad7b2f2ab14623ddc9d1ed6
CE5RIC <cedricdierckx5@hotmail.com>
2022-09-27 15:48:31 +0200

    Router and routes created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import Form from './components/common/Form'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
+      <Router>
     <div className="App">
-<Form />
+        <>
+            <Routes>
+                <Route path='/login' element={<Form />} />
+                <Route path='/register' element={<Form />} />
+            </Routes>
+        </>
     </div>
+      </Router>
   );
 }
 
