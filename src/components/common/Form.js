25ce33df1accddfbfd06565659099bc18e7f508f
CE5RIC <cedricdierckx5@hotmail.com>
2022-09-27 16:02:01 +0200

    Seperate login and register screens created

--- src/components/common/Form.js
+++ src/components/common/Form.js
@@ -3,11 +3,11 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from './Button'
 
-export default function BasicTextFields() {
+export default function BasicTextFields({title}) {
     return (
         <div>
             <div className="heading-container">
-                <h3>   Login Form
+                <h3>   {title} Form
 
                 </h3>
             </div>
@@ -23,7 +23,7 @@ export default function BasicTextFields() {
                 <TextField id="email" label="Enter the Email" variant="outlined" />
                 <TextField id="password" label="Enter the Password" variant="outlined" />
             </Box>
-            <Button />
+            <Button title={title} />
         </div>
     );
 }
